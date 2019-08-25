ad4eb5f725be23d2eb2c5e91847623a3b69fe88d
itsv_chenzhx <674595945@qq.com>
2019-08-25 10:19:37 +0800

    these codes are not mine

--- src/main/java/com/czx/immortal/common/strategy/IStudy.java
+++
@@ -1,9 +0,0 @@
-package com.czx.immortal.common.strategy;
-
-/**
- * @author chenzx
- * 修炼接口
- */
-public interface IStudy {
-    void increaseExperience();
-}
--- src/main/java/com/czx/immortal/entity/identity/Identity.java
+++
@@ -1,9 +0,0 @@
-package com.czx.immortal.entity.identity;
-
-/**
- * @author : chenzhx
- * @since : 2019/6/30/030
- **/
-public interface Identity {
-    void increaseAttribute();
-}
